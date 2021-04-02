0324380e1ab908aad89efcdb87c411b7521ed03e
Rafael Carneiro <rcrs4@cin.ufpe.br>
2021-04-02 14:55:44 -0300

    Melhorias no recv e send

--- protocolo.py
+++ protocolo.py
@@ -5,11 +5,14 @@ class Protocolo:
     def __init__(self):
         self.host = socket.gethostname()
 
-    def make_header(self):
-        raise NotImplementedError
+    def make_header(self, option, msg_size):
+        return bytes(option, 'utf-8') + msg_size.to_bytes(2, 'big')
 
-    def make_packet(self):
-        raise NotImplementedError
+    def make_packets(self, header, msg):
+        total_packet = header + bytes(msg, 'utf-8')
+        packet_size = 2048
+        packets_list = [total_packet[i:i+packet_size] for i in range(0, len(total_packet), packet_size)]
+        return packets_list
 
     def send_packet(self):
         raise NotImplementedError
@@ -48,11 +51,27 @@ class Server(Protocolo):
         client_socket = self.connection.accept()
         return client_socket
     
-    def send_packet(self, client_socket, packet):
-        client_socket.send(packet)
+    def send_packet(self, client_socket, packets):
+        for packet in packets:
+            print(packet)
+            sent = client_socket.send(packet)
+            if sent == 0:
+                raise RuntimeError("Socket connection broken")
 
     def recv_packet(self, client_socket):
-        return client_socket.recv(2048)
+        packet = client_socket.recv(2048)
+        header = packet[:3]
+        print(header)
+        msg_len = int.from_bytes(header[1:3], 'big')
+        msg_type = header[0]
+        len_recv = len(packet[2:])
+        msg_recv = [packet[2:].decode('utf-8')]
+        print(len_recv, msg_len)
+        while len_recv < msg_len:
+            msg = client_socket.recv(2048)
+            len_recv += len(msg)
+            msg_recv.append(msg.decode('utf-8'))
+        return ''.join(msg_recv)
 
 class Client(Protocolo):
     def __init__(self):
@@ -70,8 +89,22 @@ class Client(Protocolo):
     def generate_mac_key(self):
         raise NotImplementedError
     
-    def send_packet(self, packet):
-        self.connection.send(packet)
+    def send_packet(self, packets):
+        for packet in packets:
+            sent = self.connection.send(packet)
+            if sent == 0:
+                raise RuntimeError("Socket connection broken")
     
     def recv_packet(self):
-        return self.connection.recv(2048)
+        packet = self.connection.recv(2048)
+        header = packet[:3]
+        print(header)
+        msg_len = int.from_bytes(header[1:3], 'big')
+        msg_type = header[0]
+        len_recv = len(packet[2:])
+        msg_recv = [packet[2:].decode('utf-8')]
+        while len_recv < msg_len:
+            msg = self.connection.recv(2048)
+            len_recv += len(msg)
+            msg_recv.append(msg.decode('utf-8'))
+        return ''.join(msg_recv)
